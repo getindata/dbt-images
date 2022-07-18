755eec177d3ecde97cc832b469cd186aec9d97e8
Piotr Sierkin <piotr.sierkin@getindata.com>
2022-07-18 11:09:57 +0200

    chore: update bash script with chages from dbt-airflow-factory

--- executor_with_test_reports_ingestions.sh
+++ executor_with_test_reports_ingestions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-echo 'Running Script'
 array_args=( "$@" )
-if [ "${array_args[2]}" == "test" ] && test -n "${DATAHUB_GMS_URL-}"; then
-        echo  dbt "${array_args[@]:2}"
-        dbt "${array_args[@]:2}"
-        echo "${DATAHUB_GMS_URL}"
+echo 'Executing DBT with following parameters:' "${array_args[@]}"
+if [ "${array_args[0]}" == "test" ] && test -n "${DATAHUB_GMS_URL-}"; then
+        echo dbt "${array_args[@]}"
+        dbt "${array_args[@]}"
+        echo 'Sending tests reports to DataHub:' "${DATAHUB_GMS_URL}"
         datahub ingest -c datahub_assertions.yml
     else
-        echo "${array_args[@]}"
-        "${array_args[@]}"
+        echo dbt --no-write-json "${array_args[@]}"
+        dbt --no-write-json "${array_args[@]}"
     fi
 echo 'End of the script'
