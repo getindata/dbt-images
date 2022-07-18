d2a9b1db2a8ec2d18845a77ad0ebc510434b6d9c
Piotr Sierkin <piotr.sierkin@getindata.com>
2022-07-18 12:44:25 +0200

    chore: refactor bash script

--- executor_with_test_reports_ingestions.sh
+++ executor_with_test_reports_ingestions.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
-array_args=( "$@" )
+array_args=("$@")
 echo 'Executing DBT with following parameters:' "${array_args[@]}"
+dbt "${array_args[@]}"
 if [ "${array_args[0]}" == "test" ] && test -n "${DATAHUB_GMS_URL-}"; then
-        echo dbt "${array_args[@]}"
-        dbt "${array_args[@]}"
-        echo 'Sending tests reports to DataHub:' "${DATAHUB_GMS_URL}"
-        datahub ingest -c datahub_assertions.yml
-    else
-        echo dbt --no-write-json "${array_args[@]}"
-        dbt --no-write-json "${array_args[@]}"
-    fi
+    echo 'Sending tests reports to DataHub:' "${DATAHUB_GMS_URL}"
+    datahub ingest -c datahub_assertions.yml
+fi
 echo 'End of the script'
