89b243380cc44742a02332190609723348354a74
Piotr Sierkin <piotr.sierkin@getindata.com>
2022-07-29 12:24:50 +0200

    chore: refactor script

--- executor_with_test_reports_ingestions.sh
+++ executor_with_test_reports_ingestions.sh
@@ -2,13 +2,8 @@
 array_args=("$@")
 echo 'Executing DBT with following parameters:' "${array_args[@]}"
 array_args=("$@")
-if dbt "${array_args[@]}" ; then
-    exit_code=0
-    echo "Command succeeded"
-else
-    exit_code=1
-    echo "Command failed"
-fi
+dbt "${array_args[@]}"
+exit_code=$?
 if [ "${array_args[0]}" == "test" ] && test -n "${DATAHUB_GMS_URL-}"; then
     echo 'Sending tests reports to DataHub:' "${DATAHUB_GMS_URL}"
     datahub ingest -c datahub_assertions.yml
