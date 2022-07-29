dac69c5f493b916609c09639bfb2ed8d50c617c5
Piotr Sierkin <psierkin@gmail.com>
2022-07-29 11:49:08 +0200

    Repair script vol2 (#4)
    * fix: remove set -e from script
    
    * chore: fix script vol2
    
    Co-authored-by: Piotr Sierkin <piotr.sierkin@getindata.com>

--- executor_with_test_reports_ingestions.sh
+++ executor_with_test_reports_ingestions.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 array_args=("$@")
 echo 'Executing DBT with following parameters:' "${array_args[@]}"
-dbt "${array_args[@]}"
+array_args=("$@")
+if dbt "${array_args[@]}" ; then
+    exit_code=0
+    echo "Command succeeded"
+else
+    exit_code=1
+    echo "Command failed"
+fi
 if [ "${array_args[0]}" == "test" ] && test -n "${DATAHUB_GMS_URL-}"; then
     echo 'Sending tests reports to DataHub:' "${DATAHUB_GMS_URL}"
     datahub ingest -c datahub_assertions.yml
 fi
 echo 'End of the script'
+exit $exit_code
